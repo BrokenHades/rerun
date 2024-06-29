f1782ea9cb6887b241b3d979ab57836308430109
Hades <88635546+BrokenHades@users.noreply.github.com>
2024-06-29 00:45:30 +0000

    Pending changes exported from your codespace

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//First Js code
+console.log('Hello World!');
+
+// let name = 'Hades';
+// console.log(name);
+// // Person Object
+// let person = {
+//         name: 'Hades',
+//         age: '20'
+// };
+// // Dot Notation
+// person.name = 'Hadess';
+
+// console.log(person)
+
+// arrays
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors);
+
+// Functions
+
+function greet(name, lastName) {
+      console.log('Hello ' + name + '' + lastName);
+}
+
+greet('Hades', 'Queen');
+alert('hello world')
